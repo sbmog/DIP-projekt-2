e1b0a0febaab4b47e80d0d48231faaff4b911299
sbmog <eaa24sbmog@students.eaaa.dk>
2025-11-19 09:25:38 +0100

    endpoints tomme men sat ind

--- app.js
+++ app.js
@@ -8,9 +8,35 @@ const port = 8080
 app.set('view enging', 'pug')
 app.use(express.static('assets'))
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
+app.get('/chats', (request, response) => {
 
+})
+
+app.get('/chats/:id', (request, response) => {
+
+})
+
+app.get('/chats/:id/messages', (request, response) => {
+
+})
+
+app.get('/chats/messages/:id', (request, response) => {
+
+})
+
+app.get('/users', (request, response) => {
+
+})
+
+app.get('/users/:id', (request, response) => {
+
+})
+
+app.get('/users/:id/messages', (request, response) => {
+
+})
 
 app.listen(port, () => {
     console.log(`Server is listening at http://localhost:${port}`)
