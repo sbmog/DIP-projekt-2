48387d8c2f8425d1a8f587c041ed38e9dac9f232
Rune <eaa24rhje@students.eaaa.dk>
2025-11-19 11:02:37 +0100

    start på endpoints

--- app.js
+++ app.js
@@ -11,14 +11,24 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/chats', (request, response) => {
-
+    response.json(chats)
 })
 
 app.get('/chats/:id', (request, response) => {
+    const id = parseInt(request.params.id)
 
+    //Find den specifikke chat
+    const chat = chats.find(chat => chat.id === id)
+
+    //Hvis chat ikke findes
+    if (!chat){
+        return response.status(404).json({error: "Chat ikke fundet"})
+    }
+    response.json(chat)
 })
 
 app.get('/chats/:id/messages', (request, response) => {
+    const id = parseInt(request.params.id)
 
 })
 
@@ -27,11 +37,20 @@ app.get('/chats/messages/:id', (request, response) => {
 })
 
 app.get('/users', (request, response) => {
-
+    response.json(users)
 })
 
 app.get('/users/:id', (request, response) => {
+    const id = parseInt(request.params.id)
+    
+    //Find den specifikke user
+    const user = users.find(user => user.id === id)
 
+    //Hvis user ikke findes
+    if (!user) {
+        return response.status(404).json({error: "User ikke fundet"})
+    }
+    response.json(user)
 })
 
 app.get('/users/:id/messages', (request, response) => {
